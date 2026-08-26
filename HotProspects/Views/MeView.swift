587e4b596bf1d7c876b356c45927693d2aac47e9
JonasMahlburg <darrylmanga@yahoo.com>
2026-08-26 09:45:29 +0200

    feat: added share function - added context-menu to QR-Code - add 'updateQRCode'-method - refactor Code for QR-Code
    note: QRCode can be saved as an Image

--- HotProspects/Views/MeView.swift
+++ HotProspects/Views/MeView.swift
@@ -11,6 +11,7 @@ import CoreImage.CIFilterBuiltins
 struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -26,15 +27,23 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n \(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateQRCode)
+            .onChange(of: name, updateQRCode)
+            .onChange(of: emailAddress, updateQRCode)
         }
     }
+    
+    //MARK: - QR-Code Methods
     
     func generateQRCode(from string: String) -> UIImage {
         filter.message = Data(string.utf8)
@@ -42,9 +51,14 @@ struct MeView: View {
         if let outputImage = filter.outputImage {
             if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
                 return UIImage(cgImage: cgImage)
+                 
             }
         }
         return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
+    func updateQRCode(){
+        qrCode = generateQRCode(from: "\(name)\n \(emailAddress)")
     }
 }
 
