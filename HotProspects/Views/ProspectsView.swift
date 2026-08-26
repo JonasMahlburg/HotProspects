dfbf300fa9f08a397fbbd1d8877128bff94291e4
JonasMahlburg <darrylmanga@yahoo.com>
2026-08-26 10:11:45 +0200

    fest: add Notifications - import UserNotifications - implement 'addNotification'-method - notifications added to swipeActions

--- HotProspects/Views/ProspectsView.swift
+++ HotProspects/Views/ProspectsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import SwiftData
 import CodeScanner
 import AVFoundation
+import UserNotifications
 
 struct ProspectsView: View {
     enum FilterType {
@@ -55,6 +56,11 @@ struct ProspectsView: View {
                             prospect.isContacted.toggle()
                         }
                         .tint(.green)
+                        
+                        Button("Remind Me", systemImage: "bell") {
+                            addNotification(for: prospect)
+                        }
+                        .tint(.orange)
                     }
                 }
                 .tag(prospect)
@@ -91,6 +97,8 @@ struct ProspectsView: View {
         }
     }
     
+    //MARK: - prospect handler methods
+    
     init(filter: FilterType) {
         self.filter = filter
         
@@ -122,6 +130,42 @@ struct ProspectsView: View {
     func delete() {
         for prospect in selectedProspects {
             modelContext.delete(prospect)
+        }
+    }
+    
+    //MARK: - Notification methods
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            
+            // for testing purposes
+//             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            
+            center .add(request)
+        }
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else if let error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
         }
     }
 }
